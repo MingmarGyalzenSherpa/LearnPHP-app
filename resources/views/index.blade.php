d53ed27449195cac37319c5a8e42201826cdf0c0
Mingmar Gyalzen Sherpa <mingmagsherpa6@gmail.com>
2022-09-24 18:18:31 +0545

    created project

---
+++ resources/views/index.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+</head>
+<body>
+    <nav>
+        <header>
+            <h1>Programming in <span class="php">PHP</span></h1>
+        </header>
+        <div class="search-bar">
+            <input type="text" name="search" id="search" placeholder="Search">
+        </div>
+    </nav>
+    <section class="container grid">
+        fdas
+    </section>
+</body>
+</html>
